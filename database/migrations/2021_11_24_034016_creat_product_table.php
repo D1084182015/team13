6a1e1e0a39fd1d4ef92be738790f7ea0a837a6ae
DESKTOP-UITHU5S\F309 <d1084182015@gm.lhu.edu.tw>
2021-11-24 12:01:51 +0800

    date

---
+++ database/migrations/2021_11_24_034016_creat_product_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->date('transaction_date')->comment('交易日期');
+            $table->string('product',191)->comment('農產品種類');
+            $table->tinyInteger('mid')->unsigned()->comment('交易市場');
+            $table->double('high_price')->unsigned()->comment('上價');
+            $table->double('midium_price')->unsigned()->comment('中價');
+            $table->double('low_price')->unsigned()->comment('下價');
+            $table->double('average_price')->unsigned()->comment('平均價');
+            $table->integer('trading_volume')->unsigned()->comment('交易量');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
---
+++ database/migrations/2021_11_24_034137_creat_markets_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatMarketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('markets', function (Blueprint $table) {
+            $table->id();
+            $table->string('product',100)->comment('市場');
+            $table->string('product',100)->comment('區別');
+            $table->string('product',100)->comment('地址');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('markets');
+    }
+}
