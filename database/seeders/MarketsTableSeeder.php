ebe1a95cd2be1e4bdbc475d0aea369caf815c28f
DESKTOP-UITHU5S\F309 <d1084182015@gm.lhu.edu.tw>
2021-11-24 17:34:46 +0800

    random date

--- database/seeders/MarketsTableSeeder.php
+++ database/seeders/MarketsTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 
 class MarketsTableSeeder extends Seeder
@@ -12,13 +13,43 @@ class MarketsTableSeeder extends Seeder
      *
      * @return void
      */
+    public function generateRandomZone() {
+        $zones = ['東區', '北區','中區','南部'];
+        return $zones[rand(0, count($zones)-1)];
+    }
+
+    public function generateRandomMarket() {
+        $markets = ['台北一', '台北二','板橋區','三重區','宜蘭市','台中市','鳳山區','嘉義市'];
+        return $markets[rand(0, count($markets)-1)];
+    }
+
+    public function generateRandomaddress() {
+        $address = ['萬大路1009號', '北港路251號','文中路107號','山西路316號','板城路1000號','中清路二段1303號'];
+        return $address[rand(0, count($address)-1)];
+    }
+
+
+
+
     public function run()
     {
         //
-        DB::table('markets')->insert([
-            'market' =>'桃園',
-            'zone' =>'北區',
-            'address'=>'000號'
+        for($i=0; $i<25; $i++){
+
+            $market = $this->generateRandomMarket();
+            $address = $this->generateRandomaddress();
+            $zone = $this->generateRandomZone();
+            $random_datetime = Carbon::now()->subMinutes(rand(1, 55));
+
+
+
+            DB::table('markets')->insert([
+                'market' =>$market,
+                'zone' =>$zone,
+                'address'=>$address,
+                'created_at' => $random_datetime,
+                'updated_at' => $random_datetime
             ]);
+        }
     }
 }
--- database/seeders/ProductsTableSeeder.php
+++ database/seeders/ProductsTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 
 class ProductsTableSeeder extends Seeder
@@ -12,18 +13,38 @@ class ProductsTableSeeder extends Seeder
      *
      * @return void
      */
+
+
+    public function generateRandomproduct() {
+        $products = ['香蕉', '蘋果','釋迦','巨峰葡萄'];
+        return $products[rand(0, count($products)-1)];
+    }
+
+
+
+
     public function run()
     {
         //
-        DB::table('products')->insert([
-            'transaction_date'=>'100/11/10',
-            'product'=>'banana',
-            'mid'=>'1',
-            'high_price'=>'48.8',
-            'midium_price'=>'38.8',
-            'low_price'=>'26.8',
-            'average_price'=>'38.4',
-            'trading_volume'=>'15487'
-        ]);
+       for ($i=0;$i<500;$i++)
+       {
+           $transaction_date = Carbon::now()->subYears(rand(0, 2))->subMonths(rand(0, 12))->subRealDays(rand(0,31));
+           $product = $this->generateRandomproduct();
+           $random_datetime = Carbon::now()->subMinutes(rand(1, 55));
+
+
+           DB::table('products')->insert([
+               'transaction_date'=>$transaction_date,
+               'product'=>$product,
+               'mid'=>rand(1,4),
+               'high_price'=>rand(67,98),
+               'midium_price'=>rand(32,75),
+               'low_price'=>rand(10,30),
+               'average_price'=>rand(50,70),
+               'trading_volume'=>rand(1635,19453),
+               'created_at' => $random_datetime,
+               'updated_at' => $random_datetime
+           ]);
+       }
     }
 }
