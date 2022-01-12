da92e3a38d284a1b0ec8beecd543c57da000d647
DESKTOP-UITHU5S\F309 <d1084182015@gm.lhu.edu.tw>
2022-01-12 10:36:27 +0800

    //

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('products/senior', [ProductsController::class, 'senior'])->name('products.senior');
-Route::get('products/banana', [ProductsController::class, 'banana'])->name('products.banana');
-Route::get('products/apple', [ProductsController::class, 'apple'])->name('products.apple');
-Route::get('products/sakyamuni', [ProductsController::class, 'sakyamuni'])->name('products.sakyamuni');
-Route::get('products/kyohogrape', [ProductsController::class, 'kyohogrape'])->name('products.kyohogrape');
-Route::get('markets/north', [MarketsController::class, 'north'])->name('markets.north');
-Route::get('markets/south', [MarketsController::class, 'south'])->name('markets.south');
-Route::get('markets/middle', [MarketsController::class, 'middle'])->name('markets.middle');
-Route::get('markets/east', [MarketsController::class, 'east'])->name('markets.east');
+Route::get('banana', [ProductsController::class, 'banana'])->name('products.banana');
+Route::get('apple', [ProductsController::class, 'apple'])->name('products.apple');
+Route::get('sakyamuni', [ProductsController::class, 'sakyamuni'])->name('products.sakyamuni');
+Route::get('kyohogrape', [ProductsController::class, 'kyohogrape'])->name('products.kyohogrape');
+Route::get('north', [MarketsController::class, 'north'])->name('markets.north');
+Route::get('south', [MarketsController::class, 'south'])->name('markets.south');
+Route::get('middle', [MarketsController::class, 'middle'])->name('markets.middle');
+Route::get('east', [MarketsController::class, 'east'])->name('markets.east');
 Route::resource("products",ProductsController::class);
 Route::resource("markets",MarketsController::class);
